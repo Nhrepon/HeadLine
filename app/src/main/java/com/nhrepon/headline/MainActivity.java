286223f33fce3c31247167e4e5aa7e049a0191db
nhrepon <nurhossainrepon7248@gmail.com>
2024-05-29 10:01:19 +0600

    HeadLine v-1.0

--- app/src/main/java/com/nhrepon/headline/MainActivity.java
+++ app/src/main/java/com/nhrepon/headline/MainActivity.java
@@ -2,6 +2,7 @@ package com.nhrepon.headline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivities(new Intent[]{new Intent(MainActivity.this, Home.class)});
 
     }
 }
