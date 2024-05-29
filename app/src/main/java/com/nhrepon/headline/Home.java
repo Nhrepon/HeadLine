e503d9c2da5edf1127bb23109f845f93063138b3
nhrepon <nurhossainrepon7248@gmail.com>
2024-05-29 13:33:55 +0600

    add fragmentation

--- app/src/main/java/com/nhrepon/headline/Home.java
+++ app/src/main/java/com/nhrepon/headline/Home.java
@@ -1,13 +1,19 @@
 package com.nhrepon.headline;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
 public class Home extends AppCompatActivity {
@@ -16,6 +22,8 @@ public class Home extends AppCompatActivity {
     MaterialToolbar materialToolbar;
     FrameLayout frameLayout;
     NavigationView navigationView;
+
+    BottomNavigationView bottomNavigationView;
 
 
     @Override
@@ -27,10 +35,85 @@ public class Home extends AppCompatActivity {
         materialToolbar = findViewById(R.id.materialToolBar);
         frameLayout = findViewById(R.id.frameLayout);
         navigationView = findViewById(R.id.navigationBar);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
 
+
+        // navigation bar toggler
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(Home.this, drawerLayout, materialToolbar,
                 R.string.drawer_open,R.string.drawer_close);
         drawerLayout.addDrawerListener(toggle);
+
+
+      // Navigation view controller
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId()==R.id.navMenuTrending){
+
+                    // Fragmentation for trending
+                    FragmentManager fragmentManager=getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                    fragmentTransaction.add(R.id.frameLayout, new TrendingFragment());
+                    fragmentTransaction.commit();
+
+                }else if(menuItem.getItemId()==R.id.navMenuMostpopular){
+
+                    // Fragmentation for trending
+                    FragmentManager fragmentManager=getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                    fragmentTransaction.add(R.id.frameLayout, new MostPopularFragment());
+                    fragmentTransaction.commit();
+
+                }
+                return true;
+            }
+        });
+
+
+
+
+
+
+
+        // Icon badge for bottom navigation
+        bottomNavigationView.getOrCreateBadge(R.id.bottomNamTrending).setNumber(44);
+
+
+
+        // Bottom navigation controller
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId()==R.id.bottomNavHome){
+
+                    // Fragmentation for home
+                    FragmentManager fragmentManager=getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                    fragmentTransaction.add(R.id.frameLayout, new HomeFragment());
+                    fragmentTransaction.commit();
+
+                }else if(menuItem.getItemId()==R.id.bottomNamTrending){
+
+                    // Fragmentation for trending
+                    FragmentManager fragmentManager=getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                    fragmentTransaction.add(R.id.frameLayout, new TrendingFragment());
+                    fragmentTransaction.commit();
+
+
+                }else if(menuItem.getItemId()==R.id.bottomNavBookmark){
+
+                    // Fragmentation for bookmark
+                    FragmentManager fragmentManager=getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                    fragmentTransaction.add(R.id.frameLayout, new BookmarkFragment());
+                    fragmentTransaction.commit();
+
+                }
+
+                return true;
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/nhrepon/headline/HomeFragment.java
@@ -0,0 +1,37 @@
+package com.nhrepon.headline;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myView= inflater.inflate(R.layout.fragment_home, container, false);
+
+        return myView;
+    }
+
+}
